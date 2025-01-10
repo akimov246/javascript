af6822e825750ee0e52621109505c2b1c2f24661
akimov246 <akimov246@yandex.ru>
2025-01-10 16:13:06 +0700

    11. промисы, async/await. 2. промисы

---
+++ 11. Промисы, async, await/2. Промисы/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Задачи
+
+// Задержка на промисах
+function delay(ms) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, ms);
+    });
+}
+
+delay(3000).then(() => console.log('выполнилось через 3 секунды'));
