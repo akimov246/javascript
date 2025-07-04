4a0148e843733c68f32e6eaad8bd730328efa589
akimov246 <akimov246@yandex.ru>
2025-07-05 01:07:36 +0700

    6. Разное. 2. selection и range

--- highlightElements.js
+++ highlightElements.js
@@ -7,11 +7,10 @@ document.head.append(prismStyle);
 
 const prismScript = document.createElement('script');
 prismScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js';
+document.head.append(prismScript);
 
 prismScript.addEventListener('load', function() {
     document.querySelectorAll('code').forEach((elem) => {
         Prism.highlightElement(elem);
     });
 });
-
-document.head.append(prismScript);
