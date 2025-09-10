00ecddc3e4d812fb494a2b68a3a0322642e881dc
akimov246 <akimov246@yandex.ru>
2025-09-11 01:23:27 +0700

    Регулярные выражения. 1. Введение: шаблоны и флаги

--- highlightElements.js
+++ highlightElements.js
@@ -14,3 +14,32 @@ prismScript.addEventListener('load', function() {
         Prism.highlightElement(elem);
     });
 });
+
+document.querySelectorAll('pre.executable').forEach((elem) => {
+    let button = document.createElement('button');
+    button.innerText = '▶';
+    button.style.cssText = `
+        position: absolute;
+        right: 0;
+        top: 0;
+        height: 2.5rem;
+        width: 2.5rem;
+        background-color: darkgrey;
+        border: none;
+        color: white;
+        cursor: pointer;
+        opacity: 0.7;
+    `;
+
+    button.addEventListener('mouseenter', function() {
+        button.style.filter = 'brightness(1.1)';
+    });
+
+    button.addEventListener('mouseleave', function() {
+        button.style.filter = '';
+    });
+
+    elem.append(button);
+
+    elem.dispatchEvent(new CustomEvent('buttonAdded'));
+});
